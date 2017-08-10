673f9e106663ece0e560770112c6dcbe93a1c26c
niujianlong <286395446@qq.com>
2017-08-10 16:11:26 +0800

    add invoke hello

---
+++ workspace/device_node_linux_moudle/invoke_hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+int main(int argc, char const *argv[])
+{
+	int fd;
+	const char *hello_node = "hello_ctrl";
+	if((fd = open(hello_node,O_RDWR|O_NDELAY))<0){
+		printf("hello open %s failed!\n", hello_node);
+	}
+	else{
+		printf("open %s success\n",hello_node);
+	}
+	close(fd);
+	return 0;
+}
